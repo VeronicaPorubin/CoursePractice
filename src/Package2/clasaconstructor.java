abb66ec7aead97120d7d9a6859041c4f2a3c6d93
Veronica Porubin <varvaraporubin@mail.ru>
2022-01-31 17:56:17 +0200

    am creat un package

---
+++ src/Package2/clasaconstructor.java
@@ -0,0 +1,14 @@
+package Package2;
+
+public class clasaconstructor {
+    String name;
+    String surname;
+    int age;
+    clasaconstructor (String name , String surname , int age ){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+
+    }
+
+}
---
+++ src/Package2/clasamain.java
@@ -0,0 +1,12 @@
+package Package2;
+
+public class clasamain {
+    public static void main (String [] args){
+        clasaconstructor clasaconstructor = new clasaconstructor("Veronica " , "Porubin " , 27 );
+        System.out.print (clasaconstructor. name );
+        System.out.print (clasaconstructor. surname );
+        System.out.print (clasaconstructor. age );
+
+    }
+
+}
