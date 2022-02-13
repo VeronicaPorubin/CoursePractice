8d2fade7c425be84afe234f164432b478e157d52
Veronica Porubin <varvaraporubin@mail.ru>
2022-02-13 18:28:01 +0200

    HommeWork 8

--- src/Lesson8/HomeWork/CoppyElementsArray.java
+++ src/Lesson8/HomeWork/CoppyElementsArray.java
@@ -2,6 +2,18 @@ package Lesson8.HomeWork;
 
 public class CoppyElementsArray {
     public static void main(String[] args) {
+        int[] array1 = {9, 6, 3, 8, 2, 5};
+        int[] array2 = new int[array1.length];
 
+        System.out.print("Elements of original array: ");
+        for (int a = 0; a < array1.length; a++) {
+            System.out.print(array1[a] + " ");
+        }
+        System.out.println();
+        System.out.print ("Elements of coppy array: ");
+        for (int b = 0; b < array2.length; b++) {
+            System.out.print(array1[b] + " ");
+        }
     }
 }
+
--- src/Lesson8/HomeWork/MaximMinimArray.java
+++ src/Lesson8/HomeWork/MaximMinimArray.java
@@ -6,11 +6,11 @@ public class MaximMinimArray {
     public static void main(String[] args) {
 
         int intArr[] = {34, -64, 3, 23, -12, 85, 19, -7, 69};
-        // assign first array element to two variables
+
         int maximValue = 0 ;
         int minimValue = 0;
-        // iterate and compare from array index 1
-        for(int i = 1; i < intArr.length; i++){
+
+        for(int i = 0; i < intArr.length; i++){
             if(maximValue < intArr[i]){
                 maximValue = intArr[i];
             }else if(minimValue > intArr[i]){
